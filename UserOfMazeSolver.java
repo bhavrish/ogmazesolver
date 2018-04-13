48039a442b3e98cda085c63c462928f5f8c6ef86
gshey <34137978+gshey@users.noreply.github.com>
2018-04-13 13:07:14 -0400

    Update UserOfMazeSolver.java

--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -1,9 +1,8 @@
 public class UserOfMazeSolver {
 
     public static void main(String[] commandLine) throws java.io.FileNotFoundException {
-        System.out.println();
         Maze maze = new Maze( commandLine[0], Integer.parseInt( commandLine[1]), Integer.parseInt( commandLine[2]));
-        System.out.println( maze + System.lineSeparator());
+
         MazeSolver test = new MazeSolver();
         System.out.println(test.recursiveSolve(maze));
         System.out.println(test);
