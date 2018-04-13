c4578fce5a08a27e9e49c01c1ce1d147468d5a0a
Bhavesh Shah <bhavrish@gmail.com>
2018-04-13 08:41:59 -0400

    Update MazeSolver.java

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,42 +1,32 @@
 import java.util.Scanner;
 
 public class MazeSolver {
-    public final static int TREASURE = 0;
-    public final static int WALL = 1;
-    public final static int STEPPING_STONE = 2;
+    private final static int[] dirlist = {Maze.EAST, Maze.NORTH, Maze.WEST, Maze.SOUTH};
     
-int[][] maze = new int[width][height];
-boolean[][] wasHere = new boolean[width][height];
-boolean[][] correctPath = new boolean[width][height];
-int startX, startY;
-int endX, endY; 
-
-
-public boolean recursiveSolve(int x, int y) {
-    if (x == endX && y == endY) return true;
-    else if (maze[x][y] == 2 || wasHere[x][y]) return false;  
-    else {
-        wasHere[x][y] = true;
-        if (x != 0)
-            if (recursiveSolve(x-1, y)) {
-                correctPath[x][y] = true;
-                return true;
+    public MazeSolver () {		
+		solvedMazes = new ArrayList<Maze>();
+	}
+    
+    public boolean recursiveSolve(int x, int y) {
+        if(current.explorerIsOnA() == Maze.WALL) {
+		    return false;
+		}
+		else if(current.explorerIsOnA() == Maze.TREASURE) {
+		    addSolve(current);
+		    return true;
+		}
+        else {
+            maze.dropA(Maze.WALL);
+            Maze snapshot = new Maze(maze);
+            for(int dir=0; dir<dirlist.length(); i++) {
+                maze.go(dir);	
+            if (recursiveSolve(maze)){						
+                maze= new Maze(snapshot);		
+                displayerTest(maze);
             }
-        if (x != width - 1) 
-            if (recursiveSolve(x+1, y)) {
-                correctPath[x][y] = true;
-                return true;
-            }
-        if (y != 0)
-            if (recursiveSolve(x, y-1)) {
-                correctPath[x][y] = true;
-                return true;
-            }
-        if (y != height - 1)
-            if (recursiveSolve(x, y+1)) {
-                correctPath[x][y] = true;
-                return true;
-            }
-        return false;
+            else return true;				          																							//					return true :)
+        }
+        return false;				
+            return false;
 }
 }
