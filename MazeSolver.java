a026c27640ffa68796f4fd47a4f395e809ccc2d0
Bhavesh Shah <bhavrish@gmail.com>
2018-04-13 09:14:27 -0400

    Update MazeSolver.java

--- MazeSolver.java
+++ MazeSolver.java
@@ -9,24 +9,23 @@ public class MazeSolver {
     
     public boolean recursiveSolve(int x, int y) {
         if(current.explorerIsOnA() == Maze.WALL) {
-		    return false;
-		}
-		else if(current.explorerIsOnA() == Maze.TREASURE) {
-		    addSolve(current);
-		    return true;
-		}
+		return false;
+	}
+	else if(current.explorerIsOnA() == Maze.TREASURE) {
+		addSolve(current);
+		return true;
+	}
         else {
             maze.dropA(Maze.WALL);
             Maze snapshot = new Maze(maze);
             for(int dir=0; dir<dirlist.length(); i++) {
                 maze.go(dir);	
-            if (recursiveSolve(maze)){						
+            if (recursiveSolve(maze)==false){						
                 maze= new Maze(snapshot);		
                 displayerTest(maze);
             }
             else return true;				          																							//					return true :)
         }
         return false;				
-            return false;
-}
+     }
 }
